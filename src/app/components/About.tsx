bd4ee76a15f1ea8322efb18a3d3182a1cfb90eb6
Sakasu02 <s214229y@st.go.tuat.ac.jp>
2023-11-03 00:53:55 +0900

    feat: make prototype of about

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -1,35 +1,75 @@
-import { 
-Heading, Center, Wrap, WrapItem 
-} from "@/common/components";
+import { Box, Heading, VStack, HStack, Flex, Divider, Wrap } from "@chakra-ui/react";
 
-import CustomIcon from '@/components/CustomIcon'
-
-const skills: string[] = [
-  "cplusplus", "python", "react", "css3", "html5","javascript", "nextjs",
-  "pokemon", "rust", "postgresql", "switch", "twitter",
+const experienceData = [
+  {
+    year: "2020",
+    title: "Web Developer",
+    company: "Company A",
+    description: "Developed web applications using React and Node.js.",
+  },
+  {
+    year: "2018",
+    title: "Software Engineer",
+    company: "Company B",
+    description: "Worked on software development projects using various technologies.",
+  },
+  {
+    year: "2016",
+    title: "Intern",
+    company: "Company C",
+    description: "Gained experience as an intern in software development.",
+  },
+  {
+    year: "2015",
+    title: "Intern",
+    company: "Company D",
+    description: "Started my journey as a software developer.",
+  },
 ];
 
-function SkillsIcons({ skills } : { skills: string[]} ) {
+function TimeLine() {
   return (
-    <Wrap spacing={4} justify="center">
-      {skills.map((skill, index) => (
-        <WrapItem key={index}>
-          <CustomIcon icon={skill} size={100} />
-        </WrapItem>
+    <VStack alignItems="center" spacing={0}>
+      <Box w="20px" h="20px" bg="teal.500" borderRadius="full" />
+      <Divider
+        orientation="vertical"
+        borderColor="teal.500"
+        borderWidth="2px"
+        h="100px"
+      />
+    </VStack>
+  )
+}
+
+function ExperienceTimeline() {
+  return (
+    <Box>
+      {experienceData.map((experience, index) => (                                                 
+        <HStack align="start" spacing={4} key={index}>
+           <Box>
+             <Heading size="md"> {experience.year} </Heading>
+           </Box>
+          <HStack paddingY={0}>
+           <TimeLine />
+          </HStack>
+          <Box>
+            <Heading size="md">{experience.title}</Heading>
+            <Heading size="sm" color="teal.500">{experience.company}</Heading>
+            <p>{experience.description}</p>
+          </Box>
+        </HStack>
       ))}
-    </Wrap>
+    </Box>
   );
 }
 
-export default function Skills() {
+export default function Resume() {
   return (
-    <div>
-    <Heading>
-      Skills
-    </Heading>
-    <Center>
-      <SkillsIcons skills={skills} />
-    </Center>
-    </div>
-  )
+    <Box p={8}>
+      <Heading size="lg" mb={4}>
+        My Resume
+      </Heading>
+      <ExperienceTimeline />
+    </Box>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,13 @@ import { Heading } from "@/common/components";
 import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
+import About from "@/components/About";
 
 export default async function Home() {
   return (
     <div>
-      <Heading as="h1" mb={4}>
-        新着記事
-      </Heading>
       <Hero/>
+      <About/>
       <Projects/>
       <Skills/>
     </div>
