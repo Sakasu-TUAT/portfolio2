4418685e0791d177f5a401e72e3011f0d8c1604c
Sakasu02 <s214229y@st.go.tuat.ac.jp>
2023-11-04 00:15:50 +0900

    fix: timeline bar

--- my-app/src/app/components/About.tsx
+++ my-app/src/app/components/About.tsx
@@ -1,4 +1,4 @@
-import { Box, Heading, VStack, HStack, Divider } from "@chakra-ui/react";
+import { Box, Spacer, VStack, HStack, Circle, Divider, Grid, GridItem, Heading, Text} from "@chakra-ui/react";
 import ComponentBase from "@/components/Base";
 
 const experienceData = [
@@ -6,13 +6,13 @@ const experienceData = [
     year: "2023年 7月",
     title: "ICPC2023国内予選突破",
     company: "sankaKsu",
-    description: "同年11月に行われるアジア地区予選への出場権を得た",
+    description: "同年11月に行われるアジア地区予選への出場権を獲得",
   },
   {
     year: "2023年 6月",
     title: "NHK学生ロボコン2023 準優勝",
     company: "R.U.R",
-    description: "RRの制御, ERの射出システム, LiDARを担当",
+    description: "RRの操縦, ERの射出システム, LiDARを担当",
   },
   {
     year: "2022年 6月",
@@ -24,19 +24,20 @@ const experienceData = [
     year: "2021年 4月",
     title: "東京農工大学入学",
     company: "TUAT",
-    description: "Started my journey as a software developer.",
+    description: "Hello World!",
   },
 ];
 
-function TimeLine() {
+function TimeLine({h} : {h: string}) {
   return (
-    <VStack alignItems="center" spacing={0}>
-      <Box w="20px" h="20px" bg="teal.500" borderRadius="full" />
+    <VStack alignItems="center" spacing={0} h={h}>
+      <Circle size='20px' bg="blue.500" color='white'>
+        <Circle size='10px' bg='white' color='white'></Circle>
+      </Circle>
       <Divider
         orientation="vertical"
-        borderColor="teal.500"
+        borderColor="telegram.500"
         borderWidth="2px"
-        h="100px"
       />
     </VStack>
   )
@@ -44,22 +45,32 @@ function TimeLine() {
 
 function ExperienceTimeline() {
   return (
-    <Box w="100%" alignItems="center">
-      {experienceData.map((experience, index) => (                                                 
-        <HStack align="start" spacing={4} key={index}>
-           <Box>
-             <Heading size="md"> {experience.year} </Heading>
-           </Box>
-          <HStack paddingY={0}>
-           <TimeLine />
-          </HStack>
-          <Box>
-            <Heading size="md">{experience.title}</Heading>
-            <Heading size="sm" color="teal.500">{experience.company}</Heading>
-            <p>{experience.description}</p>
-          </Box>
-        </HStack>
-      ))}
+    <Box>
+    {experienceData.map((experience, index) => (
+      <Grid
+        key={index} // 必要なキーを設定
+        templateAreas={`"year divider activity"`}
+        gridTemplateRows={'1fr'}
+        gridTemplateColumns={'2fr 30px 5fr'}
+        minH="150px" // 最小高さを150pxに設定
+        gap='1'
+        color='blackAlpha'
+        fontWeight='bold'
+        w="100%"
+      >
+      <GridItem textAlign='right' bg='' area={'year'}>
+        <Heading size="md"> {experience.year} </Heading>
+      </GridItem>
+      <GridItem pl='2' bg='' area={'divider'}>
+        <TimeLine h={"100%"}/>
+      </GridItem>
+      <GridItem pl='2' bg='' area={'activity'}>
+      <Heading size="md">{experience.title}</Heading>
+      <Heading size="md" color="teal.500">{experience.company}</Heading>
+      <Text overflowWrap="break-word"> {experience.description}</Text>
+      </GridItem>
+    </Grid>
+    ))}
     </Box>
   );
 }
